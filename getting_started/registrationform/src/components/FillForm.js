47a36e54a29581c8cb4c3a9aa941be2a939d0c63
SteveJ123 <jjanardhan@gmail.com>
2019-02-02 13:51:10 +0530

    validation added

--- getting_started/registrationform/src/components/FillForm.js
+++ getting_started/registrationform/src/components/FillForm.js
@@ -13,7 +13,12 @@ class FillForm extends React.Component {
     };
   }
 
-  handleChange(event) {
+    handleChange(event) {
+      
+        if (event.target.value === "") {
+            alert(`input ${event.target.name} is empty`);
+            return false;  
+        } 
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
@@ -24,6 +29,71 @@ class FillForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     // console.log("event", event.target);
+      var username = event.target.username.value;
+      var email = event.target.email.value;
+      var mobile = event.target.mobile.value;
+      //input box is empty
+      if (
+        username === "" ||
+        email === "" ||
+        mobile === "" ||
+        event.target.password.value === ""
+      ) {
+        alert(`input is empty`);
+        //   return false;
+      }
+
+      function userNameValidation(username){
+          var regexp = new RegExp("^[a-zA-Z]+$");
+          if (username.match(regexp)) {
+              alert("name entered is correct");
+              //   return true;
+          } else {
+              alert("enter the correct name");
+              //   return false;
+          }
+      }
+      
+
+
+      function emailValidation(email) {
+          var emailvalid = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          if (email.match(emailvalid)) {
+              alert("Email Validation: Successful.");
+                // return true;
+          } else {
+              alert("check email entered");
+                // return false;
+          }
+
+      }
+
+      function mobileValidation(mobile){
+          var phoneno = /^\d{10}$/;
+          if(mobile.match(phoneno)) {
+              alert("mobile number is correct");
+            //   return true;
+          }
+          else {
+              alert("check mobile number");
+            //   return false;
+          }
+      }
+
+      //input box all text entered or number typed with name - user name
+      if (username) {
+          userNameValidation(username);
+      }
+
+      if (email) {
+          emailValidation(email);
+          
+      }
+
+      if (mobile) {
+          mobileValidation(mobile);
+      }
+      
 
     console.log("event", this.state);
   }
@@ -33,7 +103,8 @@ class FillForm extends React.Component {
       <div>
         <form className="container" onSubmit={this.handleSubmit}>
           <h1>USER</h1>
-          <input
+                <input
+                    
             id="username"
             name="username"
             placeholder="Name"
@@ -62,7 +133,7 @@ class FillForm extends React.Component {
             name="password"
             placeholder="Password"
                     onChange={this.handleChange}
-            type="text"
+            type="password"
           />
 
           <input type="submit" id="register" value="REGISTER" />
--- getting_started/registrationform/src/components/Home.js
+++ getting_started/registrationform/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = props => {
-    return <h1>Worker Detaiils</h1>;
+    return <h1 id="homeheader">Worker Detaiils</h1>;
 };
 
 export default Home;
