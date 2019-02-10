9544c17011f827c77e75aa2606f80310c8b4f5c7
SteveJ123 <jjanardhan@gmail.com>
2019-02-10 15:46:04 +0530

    changed

--- getting_started/registrationform/src/components/Error.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Error = props => {
-  return <div>Error: The page does not exist!!!!</div>;
-};
-
-export default Error;
--- getting_started/registrationform/src/components/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = props => {
-    return <h1 id="homeheader">Worker Detaiils</h1>;
-};
-
-export default Home;
