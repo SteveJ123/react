d376b22b52260a82095739c25396b739ba96873e
unknown <jjanardhan@gmail.com>
2019-07-15 20:34:44 +0530

    countries languages added

--- getting_started/react-countries/src/App.js
+++ getting_started/react-countries/src/App.js
@@ -47,7 +47,26 @@ const [filter, setfilter] = useState('');
          <ol>{countries.map(element => <li>{element.name}</li>)}</ol>
          :null}
          { filter  ?
-         filter && filter.length<10 ?
+         filter && filter.length<10 ? filter && filter.length === 1 ?
+          <div>{filter.map(element =>{
+            return (
+              <div>
+              <div>
+              country: {element.name}
+              <h1>languages</h1>
+              <ul>{
+                element.languages.map(x =>  <li>{x.name}</li> )
+              }</ul>
+              </div>
+              <img src={element.flag}/>
+              </div>
+            )
+          }
+          
+          )
+         } 
+         
+         </div>:
          <ol>{filter.map(element => <li>{element.name}</li>)}</ol>
         // <span>test</span>
          :<span>greater than 10</span>:null }
